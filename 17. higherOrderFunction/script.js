03d1dc26f4c4ce90ff8113401be78ae4e7abdb78
Pappu Dutta <pappudutta279@gmail.com>
2023-08-28 13:15:41 +0530

    refactor: Higher order function custom map func

--- 17. higherOrderFunction/script.js
+++ 17. higherOrderFunction/script.js
@@ -1,11 +1,60 @@
-function x(name) {
-  console.log("Hi", name);
+// Example: 1
+// function x(name) {
+//   console.log("Hi", name);
+// }
+
+// //Higher Order Function takes another function
+// function sayName(myName) {
+//   x(myName);
+// }
+
+// sayName("Neerav");
+// sayName("Rubi");
+
+// Example: 2
+const radius = [2, 3, 4];
+const radius2 = [2, 5, 4];
+
+function area(radius) {
+  return Math.PI * radius * radius;
 }
 
-//Higher Order Function takes another function
-function sayName(myName) {
-  x(myName);
+function diameter(radius) {
+  return 2 * radius;
 }
 
-sayName("Neerav");
-sayName("Rubi");
+function circumference(radius) {
+  return 2 * Math.PI * radius;
+}
+
+Array.prototype.calculator = function (fn) {
+  const output = [];
+  for (let i = 0; i < this.length; i++) {
+    output.push(fn(this[i]));
+  }
+  return output;
+};
+console.log(radius);
+
+console.log(typeof radius.map(diameter));
+
+console.log(radius.calculator(diameter));
+console.log(radius2.calculator(diameter));
+
+// let num = [23, 32];
+// function double(num) {
+//   return 2 * num;
+// }
+// function triple(num) {
+//   return 3 * num;
+// }
+
+// Array.prototype.mapnew = function (logic) {
+//   let newNum = [];
+//   for (let i = 0; i < this.length; i++) {
+//     newNum.push(logic(this[i]));
+//   }
+//   return newNum;
+// };
+// console.log(num.mapnew(double));
+// console.log(num.mapnew(triple));
