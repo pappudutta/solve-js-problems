5a209e15be5aa79097c911afa9c9fc5eeb59a8cd
Pappu Dutta <pappudutta279@gmail.com>
2023-08-29 10:59:59 +0530

    create array function developed with recursion

--- 20. recursion (backtraking algorithm)/script.js
+++ 20. recursion (backtraking algorithm)/script.js
@@ -8,25 +8,50 @@
 //   return product;
 // }
 
-function multiplyArr(arr) {
-  console.log(arr);
+// function multiplyArr(arr) {
+//   console.log(arr);
 
-  if (arr.length <= 1) {
-    return 1;
-  } else {
-    return arr[arr.length - 1] * multiplyArr(arr.slice(0, arr.length - 1));
-  }
-}
+//   if (arr.length <= 1) {
+//     return 1;
+//   } else {
+//     return arr[arr.length - 1] * multiplyArr(arr.slice(0, arr.length - 1));
+//   }
+// }
 
-console.log(multiplyArr([1, 2, 4]));
+// console.log(multiplyArr([1, 2, 4]));
 
-function addArr(arr) {
-  console.log(arr);
-  if (arr.length < 1) {
-    return 0;
-  } else {
-    return arr[0] + addArr(arr.slice(1));
-  }
-}
+// function addArr(arr) {
+//   console.log(arr);
+//   if (arr.length < 1) {
+//     return 0;
+//   } else {
+//     return arr[0] + addArr(arr.slice(1));
+//   }
+// }
 
 // console.log(addArr([1, 2, 3, 4]));
+
+// ! Factorial of n
+
+// function factorial(n) {
+//   if (n === 1) {
+//     return 1;
+//   }
+//   console.log(n);
+//   return n * factorial(n - 1);
+// }
+
+// console.log(factorial(3));
+
+// ! Create an array with range of numbers
+// * input: start = 1, end = 5 , output : [1,2,3,4,5]
+
+function rangeOfNumbers(start, end) {
+  if (start > end) {
+    return [];
+  }
+  const numbers = rangeOfNumbers(start, end - 1);
+  numbers.push(end);
+  return numbers;
+}
+console.log(rangeOfNumbers(1, 3));
--- Test/script.js
+++ Test/script.js
@@ -4,43 +4,47 @@
 // Input: nums1 = [1,2], nums2 = [3,4]
 // Output: 2.50000
 
-var findMedianSortedArrays = function (nums1, nums2) {
-  //   let arr = nums1.concat(nums2);
-  //   if (arr.length < 2) {
-  //     return arr[0];
-  //   }
-  //   let sortedArr = arr.slice().sort((a, b) => a - b); // 2,3
-  //   if (arr.length < 3) {
-  //     return (arr[0] + arr[1]) / 2;
-  //   }
-  //   let length = sortedArr.length;
+// var findMedianSortedArrays = function (nums1, nums2) {
+//   let arr = nums1.concat(nums2);
+//   if (arr.length < 2) {
+//     return arr[0];
+//   }
+//   let sortedArr = arr.slice().sort((a, b) => a - b); // 2,3
+//   if (arr.length < 3) {
+//     return (arr[0] + arr[1]) / 2;
+//   }
+//   let length = sortedArr.length;
 
-  //   let middleIndex = Math.floor(arr.length / 2);
+//   let middleIndex = Math.floor(arr.length / 2);
 
-  //   if (length / 2 === 1) {
-  //     return sortedArr[middleIndex];
-  //   } else {
-  //     return (sortedArr[middleIndex] + sortedArr[middleIndex - 1]) / 2;
-  //   }
+//   if (length / 2 === 1) {
+//     return sortedArr[middleIndex];
+//   } else {
+//     return (sortedArr[middleIndex] + sortedArr[middleIndex - 1]) / 2;
+//   }
 
-  let nums = nums1.concat(nums2);
-  let numsLength = nums.length; //3
+//   let nums = nums1.concat(nums2);
+//   let numsLength = nums.length; //3
 
-  if (numsLength < 2 || numsLength < 1) {
-    return nums[0];
-  }
+//   if (numsLength < 2 || numsLength < 1) {
+//     return nums[0];
+//   }
 
-  let sortedNum = nums.slice().sort((a, b) => a - b); // 1,2,3,4
-  let middleIndex = Math.floor(numsLength / 2);
+//   let sortedNum = nums.slice().sort((a, b) => a - b); // 1,2,3,4
+//   let middleIndex = Math.floor(numsLength / 2);
 
-  if (numsLength % 2 === 1) {
-    return sortedNum[middleIndex];
-  } else {
-    return (sortedNum[middleIndex - 1] + sortedNum[middleIndex]) / 2;
-  }
-};
-// findMedianSortedArrays([-1], [3, -2]);
-console.log(findMedianSortedArrays([1, 2], [3, 4]));
-console.log(findMedianSortedArrays([], [1, 2, 3, 4, 5]));
-console.log("(2.5)", findMedianSortedArrays([1, 2], [3, 4]));
-console.log("output: (1) ", findMedianSortedArrays([3], [-2, -1]));
+//   if (numsLength % 2 === 1) {
+//     return sortedNum[middleIndex];
+//   } else {
+//     return (sortedNum[middleIndex - 1] + sortedNum[middleIndex]) / 2;
+//   }
+// };
+// // findMedianSortedArrays([-1], [3, -2]);
+// console.log(findMedianSortedArrays([1, 2], [3, 4]));
+// console.log(findMedianSortedArrays([], [1, 2, 3, 4, 5]));
+// console.log("(2.5)", findMedianSortedArrays([1, 2], [3, 4]));
+// console.log("output: (1) ", findMedianSortedArrays([3], [-2, -1]));
+
+let arr = [1, 2, 3, 4];
+let newarr = arr.slice(0, arr.length - 1);
+console.log(newarr);
